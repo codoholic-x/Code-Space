d4bd97502f10f5e8234531e5f818d2ce70242157
codoholic-x <gauravxx0524@gmail.com>
2025-04-06 15:24:27 +0530

    add two new file

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const heading = React.createElement(
+    "h1",
+
+    {
+        id: "tittle",
+    },
+
+    "heading 1"
+);
+
+const heading2 = React.createElement(
+    "h2",
+
+    {
+        id: "tittle",
+    },
+
+    "heading 2"
+);
+
+const container = React.createElement(
+    "div",
+    {
+        id: "container",
+    },
+
+    [heading, heading2]
+);
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
